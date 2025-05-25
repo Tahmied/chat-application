8e69b4135859d30bb713f2abde8f27583e26f452
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-05-25 18:20:17 +0600

    - practiced to send data to all users

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import express from "express";
-import http from "http";
-import { Server } from "socket.io";
+import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
@@ -8,23 +8,21 @@ const io = new Server(server)
 app.use(express.static('public'))
 
 app.get('/' , (req,res)=>{
-    res.send('fuck off')
+    res.send("server is started")
 })
 
 io.on('connection' , (socket)=>{
-    console.log(`ws connected ${socket.id}`)
-    socket.on('disconnect' , ()=>{
-        console.log(`ws disocnnected - ${socket.id}`)
-    } )
-    socket.on('message' , (data)=>{
-        console.log(`message - ${data}`)
-        io.emit('toFrontend' , 'message recieved')
+    console.log(`A client is connect on ${socket.id}`)
+    socket.on('toAll' , (data)=>{
+        console.log(data)
+        const response = {
+            'message' : 'data.message',
+            'user' : socket.id
+        }
+        io.emit('everyone' , response)
     })
-    
-})
-
-
+} )
 
 server.listen(8080 , ()=>{
-    console.log(`server is running on port 8080`)
+    console.log(`server is running on localhost:8080`)
 })
